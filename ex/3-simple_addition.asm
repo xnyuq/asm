110b896db0e3a9e69f892b6f4f47c320f3f16791
Quy Nguyen <youngboy5801@gmail.com>
2021-03-02 14:21:18 +0700

    ex3 completed

--- ex/3-simple_addition.asm
+++ ex/3-simple_addition.asm
@@ -1,6 +1,6 @@
 %include 'functions.asm'
 section .bss
-sinput resb 4
+num resb 20
 
 section .text
 global _start
@@ -8,22 +8,80 @@ global _start
 sscan:
     mov eax, 3
     mov ebx, 2
-    mov ecx, sinput
-    mov edx, 4
+    mov ecx, num
+    mov edx, 20
     int 80h
     ret
-toInt:
-    xor ebx, ebx
-    cmp byte [eax], 0
-    jz fns
-    mul ebx, 10
-    add 
-    inc eax
 
 fns:
-    mov eax, edx
     ret
 
+iprint:
+    xor ecx, ecx
+
+divideLoop:
+    mov edx, 0
+    mov ebx, 10
+    idiv ebx
+    inc ecx
+    push edx
+    cmp eax, 0
+    jz printLoop
+    jmp divideLoop
+
+printLoop:
+    cmp ecx, 0
+    jz fns
+    dec ecx
+    pop eax
+    add eax, 0x30
+    push eax
+    mov eax, esp
+    call sprint
+    pop edx
+    jmp printLoop
+
+atoi:
+    cmp byte [esi+ecx], '0'
+    jl fns
+    cmp byte [esi+ecx], '9'
+    jg fns
+    imul eax, 10
+    xor ebx, ebx
+    mov bl, byte [esi+ecx]
+    sub bl, '0'
+    add eax, ebx
+    inc ecx
+    jmp atoi
+
 _start:
+    ;input 1st number
     call sscan
-    mov eax, sinput
+    mov esi, num
+    xor ecx, ecx
+    xor eax, eax
+    call atoi
+    mov edx, eax
+    push edx
+
+    ;input 2nd number
+    call sscan
+    mov esi, num
+    xor ecx, ecx
+    xor eax, eax
+    call atoi
+
+    ;add and print
+    pop edx
+    add eax, edx
+    call iprint
+
+
+    ;newline
+    mov eax, 0x0A
+    push eax
+    mov eax, esp
+    call sprint
+    pop eax
+
+    call quit
